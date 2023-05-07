e49567a9885432ac4cfd3c966235116699b8ccae
shinhunwoo <shinhunwoo1122@gmail.com>
2023-05-07 23:04:15 +0900

    한양사이버대학교 졸업과제 진행중

---
+++ src/main/java/com/example/project/controller/MainController.java
@@ -0,0 +1,55 @@
+package com.example.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public ModelAndView Main(){
+
+        ModelAndView mView = new ModelAndView();
+        mView.setViewName("index");
+
+        return mView;
+    }
+
+    @GetMapping("/1")
+    public ModelAndView contact(){
+
+        ModelAndView mView = new ModelAndView();
+        mView.setViewName("contact");
+
+        return mView;
+    }
+
+    @GetMapping("/2")
+    public ModelAndView about(){
+
+        ModelAndView mView = new ModelAndView();
+        mView.setViewName("about");
+
+        return mView;
+    }
+
+    @GetMapping("/3")
+    public ModelAndView shop(){
+
+        ModelAndView mView = new ModelAndView();
+        mView.setViewName("shop");
+
+        return mView;
+    }
+
+    @GetMapping("/4")
+    public ModelAndView single(){
+
+        ModelAndView mView = new ModelAndView();
+        mView.setViewName("shop-single");
+
+        return mView;
+    }
+
+}
